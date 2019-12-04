5da5cbbaea8bd7d6b967fd024465ac171380d814
Varvara Voronina <Apesinko@rambler.ru>
2019-12-04 22:54:43 +0300

    Улучшены условия проверки на попадание в диапазон чисел

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -5,19 +5,17 @@ public class GuessTheNumber {
 		int humanNumber = 99;
 		System.out.println("\n" + humanNumber);
 
+		if (humanNumber < 0 || humanNumber > 100) {
+			System.out.println("\nВы ввели число не попадающие в интервал от 0 до 100");					
+			System.out.println("\nУгадайте число от 0 до 100");
+		}
+
 		if (humanNumber == computerNumbler){
 			System.out.println("\nВы угадали");
 		} else if (humanNumber >= computerNumbler && humanNumber < 100){
 			System.out.println("\nЗагаданное число меньше");
 		} else if (humanNumber <= computerNumbler){
 			System.out.println("\nЗагаданное число больше");
-		}
-	if (humanNumber < 0) {
-		System.out.println("\nВы ввели число не попадающее в интервал от 0 до 100");					
-		System.out.println("\nУгадайте число от 0 до 100");
-	} else if (humanNumber > 100) {
-		System.out.println("\nВы ввели число не попадающие в интервал от 0 до 100");					
-		System.out.println("\nУгадайте число от 0 до 100");
-		}
+		}		
 	}
 }
