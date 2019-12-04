f67778264ca9b0136f320c489ce3ebe280a7d288
Varvara Voronina <Apesinko@rambler.ru>
2019-12-04 16:46:38 +0300

    Сделано задание "угадай число"

---
+++ GuessTheNumber.java
@@ -0,0 +1,23 @@
+public class GuessTheNumber {
+	public static void main(String[] args) {
+		System.out.println("Угадайте число от 0 до 100");
+		int computerNumbler = 65;
+		int humanNumber = 99;
+		System.out.println("\n" + humanNumber);
+
+		if (humanNumber == computerNumbler){
+			System.out.println("\nВы угадали");
+		} else if (humanNumber >= computerNumbler){
+			System.out.println("\nЗагаданное число меньше");
+		} else if (humanNumber <= computerNumbler){
+			System.out.println("\nЗагаданное число больше");
+		}
+//		if (humanNumber > 100) {
+//			System.out.println("\nВы ввели число не попадающее в интервал от 0 до 100");					
+//			System.out.println("\nУгадайте число от 0 до 100");
+//		} else if (humanNumber < 100) {
+//			System.out.println("\nВы ввели число не попадающие в интервал от 0 до 100");					
+//			System.out.println("\nУгадайте число от 0 до 100");
+//		}
+	}
+}
