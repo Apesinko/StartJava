9b6b18d49e7c59b7c934b9b9ee33ef193a528327
Varvara Voronina <Apesinko@rambler.ru>
2019-12-12 12:28:18 +0300

    Инициализация и объявление переменных перенесены в циклы

--- Lesson 1/Unicode.java
+++ Lesson 1/Unicode.java
@@ -1,8 +1,7 @@
 public class Unicode {
 	public static void main(String[] args) {
-		System.out.println("Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. Если выводятся кракозябры, то [33, 126]");
-		char i;
-		for (i = 33; i <= 126; i++) {
+		System.out.println("Выведите в цикле символы кодировки Unicode в диапазоне [9398, 10178]. Если выводятся кракозябры, то [33, 126]");		
+		for (char i = 33; i <= 126; i++) {
 			System.out.print(i + " ");
 		}
 	}
