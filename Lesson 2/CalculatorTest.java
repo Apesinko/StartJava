1be051ecf2a89c90f6f4f91128152b40734e79a3
Varvara Voronina <Apesinko@rambler.ru>
2019-12-24 22:53:56 +0300

    Добавлены тесты калькулятора

---
+++ Lesson 2/CalculatorTest.java
@@ -0,0 +1,32 @@
+public class CalculatorTest {
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		double a = 4D;
+		double b = 2D;
+		double result; 
+	
+		// Проверям сложение
+		result = 6D;
+		System.out.println("\nТест суммирования пройден: " + (calculator.calc(a, "+", b) == result));
+		// Проверям вычитание
+		result = 2D;
+		System.out.println("\nТест вычитания пройден: " + (calculator.calc(a, "-", b) == result));
+		// Проверям умножение
+		result = 8D;
+		System.out.println("\nТест умножения пройден: " + (calculator.calc(a, "*", b) == result));
+		// Проверям деление
+		result = 2D;
+		System.out.println("\nТест деления пройден: " + (calculator.calc(a, "/", b) == result));
+		// Проверям возведение в степень
+		result = 16D;
+		System.out.println("\nТест вовзведения в степень пройден: " + (calculator.calc(a, "^", b) == result));
+		// Проверям деление с остатком
+		result = 0D;
+		System.out.println("\nТест деления с остатком пройден: " + (calculator.calc(a, "%", b) == result));
+
+		System.out.println("\n------------------------------------------------------------------------------");
+
+		// Проверяем общение с пользователем
+		calculator.userInterface();
+	}
+}
