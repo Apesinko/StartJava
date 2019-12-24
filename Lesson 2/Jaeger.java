2d441cde83ff525d96889102e232991dff9ca246
Varvara Voronina <Apesinko@rambler.ru>
2019-12-24 22:55:41 +0300

    Объеявила переменные без инициализации полей, добавлены геттеры и сеттеры (свойства полей), добавлены методы

---
+++ Lesson 2/Jaeger.java
@@ -0,0 +1,93 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private double height;
+    private double weight;
+    private int speed;
+    private int strenght;
+    private int armor;
+
+    public boolean drift() {
+        System.out.println("Вы вошли в дрифт");
+        return true;
+    }
+
+    public void move() {
+        System.out.println("Вы прошли 5 шагов");
+    }
+
+    public String scanKaijy() {
+        return "nothing";
+    }
+
+    public void useVortexCannon() {
+		System.out.println("Fatality!!!!!");
+    }
+
+    // Свойства полей
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+}
