f7fe9d53da3e0669db7a4bba9861c0b307dac9dc
Varvara Voronina <Apesinko@rambler.ru>
2019-12-26 17:54:44 +0300

    Внесены правки

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -7,26 +7,26 @@ public class CalculatorTest {
 	
 		// Проверям сложение
 		result = 6D;
-		System.out.println("\nТест суммирования пройден: " + (calculator.calc(a, "+", b) == result));
+		System.out.println("\nТест суммирования пройден: " + (calculator.calculate(a, "+", b) == result));
 		// Проверям вычитание
 		result = 2D;
-		System.out.println("\nТест вычитания пройден: " + (calculator.calc(a, "-", b) == result));
+		System.out.println("\nТест вычитания пройден: " + (calculator.calculate(a, "-", b) == result));
 		// Проверям умножение
 		result = 8D;
-		System.out.println("\nТест умножения пройден: " + (calculator.calc(a, "*", b) == result));
+		System.out.println("\nТест умножения пройден: " + (calculator.calculate(a, "*", b) == result));
 		// Проверям деление
 		result = 2D;
-		System.out.println("\nТест деления пройден: " + (calculator.calc(a, "/", b) == result));
+		System.out.println("\nТест деления пройден: " + (calculator.calculate(a, "/", b) == result));
 		// Проверям возведение в степень
 		result = 16D;
-		System.out.println("\nТест вовзведения в степень пройден: " + (calculator.calc(a, "^", b) == result));
+		System.out.println("\nТест вовзведения в степень пройден: " + (calculator.calculate(a, "^", b) == result));
 		// Проверям деление с остатком
 		result = 0D;
-		System.out.println("\nТест деления с остатком пройден: " + (calculator.calc(a, "%", b) == result));
+		System.out.println("\nТест деления с остатком пройден: " + (calculator.calculate(a, "%", b) == result));
 
 		System.out.println("\n------------------------------------------------------------------------------");
 
 		// Проверяем общение с пользователем
-		calculator.userInterface();
+		calculator.inputData();
 	}
 }
