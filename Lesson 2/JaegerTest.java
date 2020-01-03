89ad26aaebcddc02634bfb422b1cfb9674737e0d
Varvara Voronina <Apesinko@rambler.ru>
2020-01-03 22:03:19 +0300

    Один Jaeger иницилиазирован с помощью коструктура

--- Lesson 2/JaegerTest.java
+++ Lesson 2/JaegerTest.java
@@ -1,30 +1,15 @@
 public class JaegerTest {
 	public static void main(String[] args) {
-		Jaeger jaegerOne = new Jaeger();
+		Jaeger jaegerOne = new Jaeger("Tacit Ronin", "Mark-1", "Japan", 74.37D, 7.45D, 8, 7, 3);
 		Jaeger jaegerTwo = new Jaeger();
 
-		jaegerOne.setModelName("Tacit Ronin");
 		System.out.println("\nНазвание егеря: " + jaegerOne.getModelName());
-
-		jaegerOne.setMark("Mark-1");
 		System.out.println("Марка егеря: " + jaegerOne.getMark());
-
-		jaegerOne.setOrigin("Japan");
 		System.out.println("Страна производителя: " + jaegerOne.getOrigin());
-
-		jaegerOne.setHeight(74.37);
 		System.out.println("Высота егеря в метрах: " + jaegerOne.getHeight());
-
-		jaegerOne.setWeight(7.45);
 		System.out.println("Высота егеря в тоннах: " + jaegerOne.getWeight());
-
-		jaegerOne.setSpeed(8);
 		System.out.println("Скорость егеря: " + jaegerOne.getSpeed());
-
-		jaegerOne.setStrenght(7);
 		System.out.println("Прочность егеря: " + jaegerOne.getStrenght());
-
-		jaegerOne.setArmor(3);
 		System.out.println("Броня егеря: " + jaegerOne.getArmor());
 
 		jaegerOne.drift();
