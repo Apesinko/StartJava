ba638659199259f9613336f84266f1f735e0952f
Varvara Voronina <Apesinko@rambler.ru>
2020-01-12 21:02:32 +0300

    Добавлен конструктор

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class GuessNumber {
+	Player player1;
+	Player player2;
 
-	public void guess(Player player1, Player player2) { 
-		Scanner scaner = new Scanner(System.in);	
+	public GuessNumber(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+	public void guess() { 
+		Scanner scanner = new Scanner(System.in);	
 		Random random = new Random();		
 		int start = 0;
 		int end = 100;
 		int i = 0;
 		int computerNumber = random.nextInt(end);
-		int humanNumber;
 		Player player;
+		String answerContinueGame;
 
 		System.out.println("\nУгадайте число от " + start + " до " + end);
 		do{			
@@ -23,17 +30,18 @@ public class GuessNumber {
 			}
 			
 			System.out.println(player.getName() + ", введите число");
-			humanNumber = scaner.nextInt();
-			if (humanNumber >= start && humanNumber <= end) {
-				if (humanNumber >= computerNumber){
+			player.setNumber(scanner.nextInt());
+
+			if (player.getNumber() >= start && player.getNumber() <= end) {
+				if (player.getNumber() >= computerNumber){
 					System.out.println("\nЗагаданное число меньше");
-				} else if (humanNumber <= computerNumber) {
+				} else if (player.getNumber() <= computerNumber) {
 					System.out.println("\nЗагаданное число больше");
 				}
 			} else {
 				System.out.println("\nВы ввели число не попадающие в интервал от "  + start + " до " + end);
 			}
-		} while(humanNumber != computerNumber);
+		} while(player.getNumber() != computerNumber);
 		System.out.println(player.getName() + ", Вы угадали");		
 	} 
 }
