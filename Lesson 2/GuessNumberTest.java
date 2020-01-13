4a6a61f66b13eafa8bd07ab8153367b5eecb0827
Varvara Voronina <Apesinko@rambler.ru>
2020-01-13 14:08:26 +0300

    Переместила "Хотите сыграть еще раз"

--- Lesson 2/GuessNumberTest.java
+++ Lesson 2/GuessNumberTest.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);	
+    public static void main(String[] args) {
+        String answerContinueGame;
 
-		System.out.println("Введите имя первого игрока");
-		Player playerOne = new Player(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Введите имя второго игрока");
-		Player playerTwo = new Player(scanner.nextLine());
+        System.out.println("Введите имя первого игрока");
+        Player playerOne = new Player(scanner.nextLine());
 
-		GuessNumber guessNumber = new GuessNumber(playerOne, playerTwo);
-		guessNumber.guess();
+        System.out.println("Введите имя второго игрока");
+        Player playerTwo = new Player(scanner.nextLine());
 
-	}
+        GuessNumber guessNumber = new GuessNumber(playerOne, playerTwo);
+        do{
+            guessNumber.guess();
+            do {
+                System.out.println("\nХотите ещё раз сыграть? yes/no.");
+                answerContinueGame = scanner.nextLine();
+            } while (!answerContinueGame.equals("yes") & !answerContinueGame.equals("no"));
+
+        } while (answerContinueGame.equals("yes"));
+    }
 }
