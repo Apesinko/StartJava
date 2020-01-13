a5c83b254e112041f530af547de6c44bd6fe72c8
Varvara Voronina <Apesinko@rambler.ru>
2020-01-13 14:08:41 +0300

    Сделала поля приватными

--- Lesson 2/GuessNumber.java
+++ Lesson 2/GuessNumber.java
@@ -1,59 +1,50 @@
 import java.util.Scanner;
-import java.util.Random;
+        import java.util.Random;
 
 public class GuessNumber {
-	Player player1;
-	Player player2;
+    private Player player1;
+    private Player player2;
+    private int computerNumber;
 
-	public GuessNumber(Player player1, Player player2) {
-		this.player1 = player1;
-		this.player2 = player2;
-	}
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 
-	public void guess() { 
-		Scanner scanner = new Scanner(System.in);	
-		Random random = new Random();		
-		int start = 0;
-		int end = 100;
-		int i = 0;
-		int computerNumber;
-		Player player;
-		String answerContinueGame;
+    public void guess() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int start = 0;
+        int end = 100;
+        int i = 0;
+        Player player;
 
-		do {
-			System.out.println("\nУгадайте число от " + start + " до " + end);
-			computerNumber = random.nextInt(end);
+        System.out.println("\nУгадайте число от " + start + " до " + end);
+        computerNumber = random.nextInt(end);
 
-			do{			
-				i++;
-				if (i % 2 != 0) {
-					player = player1;
-				} else {
-					player = player2;
-				}
-				
-				System.out.println(player.getName() + ", введите число");
-				player.setNumber(scanner.nextInt());
+        do{
+            i++;
+            if (i % 2 != 0) {
+                player = player1;
+            } else {
+                player = player2;
+            }
 
-				if (player.getNumber() >= start && player.getNumber() <= end) {
-					if (player.getNumber() > computerNumber){
-						System.out.println("\nЗагаданное число меньше");
-					} else if (player.getNumber() < computerNumber) {
-						System.out.println("\nЗагаданное число больше");
-					}
-				} else {
-					System.out.println("\nВы ввели число не попадающие в интервал от "  + start + " до " + end);
-				}
-			} while (player.getNumber() != computerNumber);
+            System.out.println(player.getName() + ", введите число");
+            player.setNumber(scanner.nextInt());
 
-			System.out.println(player.getName() + ", Вы угадали");
-			scanner.nextLine();
+            if (player.getNumber() >= start && player.getNumber() <= end) {
+                if (player.getNumber() > computerNumber) {
+                    System.out.println("\nЗагаданное число меньше");
+                } else if (player.getNumber() < computerNumber) {
+                    System.out.println("\nЗагаданное число больше");
+                }
+            } else {
+                System.out.println("\nВы ввели число не попадающие в интервал от "  + start + " до " + end);
+            }
+        } while (player.getNumber() != computerNumber);
 
-			do {
-                System.out.println("\nХотите ещё раз сыграть? yes/no.");
-                answerContinueGame = scanner.nextLine();
-            } while (!answerContinueGame.equals("yes") & !answerContinueGame.equals("no"));
-            
-		} while (answerContinueGame.equals("yes"));
-	}	
+        System.out.println(player.getName() + ", Вы угадали");
+        scanner.nextLine();
+    }
 }
