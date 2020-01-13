267f28261b153e25ca3ed977d687884be72fcc0d
Varvara Voronina <Apesinko@rambler.ru>
2020-01-13 14:32:20 +0300

    Файл отредактиторован

--- Lesson 2/Player.java
+++ Lesson 2/Player.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 public class Player {
-	private String name;
-	private int number;
+    private String name;
+    private int number;
 
-	public Player(String name){
-		this.name = name;
-	}
-
-	public String getName() {
-  		return name;
+    public Player(String name){
+        this.name = name;
     }
 
-	public int getNumber() {
-  		return number;
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     public void setNumber(int number) {
         this.number = number;
     }
-
 }
